bcf3f650d3c410bc5ed0703debcf376a54f2c1d6
Sander Molenkamp <a.molenkamp@gmail.com>
2022-12-30 12:06:53 +0100

    WIP

--- src/deploy/build-docker-images.sh
+++ src/deploy/build-docker-images.sh
@@ -1,4 +1,4 @@
 docker build --tag amolenk/dapr-trafficcontrol-mosquitto:latest ../mosquitto
-docker build --tag amolenk/dapr-trafficcontrol-trafficcontrolservice:latest ../trafficcontrolservice
+docker build --tag amolenk/dapr-trafficcontrol-trafficcontrolservice:latest ../TrafficControlService
 docker build --tag amolenk/dapr-trafficcontrol-finecollectionservice:latest ../FineCollectionService
-docker build --tag amolenk/dapr-trafficcontrol-vehicleregistrationservice:latest ../vehicleregistrationservice
+docker build --tag amolenk/dapr-trafficcontrol-vehicleregistrationservice:latest ../VehicleRegistrationService
